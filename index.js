4c139bcce5b32be2604b5f6e235d8d3227246b98
StrayFurther <32840436+StrayFurther@users.noreply.github.com>
2018-10-17 12:51:02 +0200

    make verify token not visible

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 app.get('/webhook', (req, res) => {
 
   // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "MEIN_TEST_TOKEN";
+  let VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
     
   // Parse the query params
   let mode = req.query['hub.mode'];
