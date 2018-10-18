9a7a7b7d359a0909011942ec31fc8510be1aa23c
StrayFurther <32840436+StrayFurther@users.noreply.github.com>
2018-10-18 14:59:38 +0200

    messages limited? noppp

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post('/webhook', (req, res) => {
      //Gib sender id aus
      console.log("senderid: "+entry['messaging'][0]['sender']['id']+ " \n");
 
-        sendTestAnswer(864272057029821);
+       // sendTestAnswer(864272057029821);
 
     });
 
