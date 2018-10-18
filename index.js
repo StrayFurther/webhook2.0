56fe8ce863ffbe7ee1fbcba9f35be0f8e011e919
StrayFurther <32840436+StrayFurther@users.noreply.github.com>
2018-10-18 17:50:54 +0200

    send not magic number id

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i]
-      let sender = event.sender.id
+      let sender = "1228516987273545";
       console.log("Sender:  "+sender+" type:"+typeof(sender));
       if (event.message && event.message.text) {
         let text = event.message.text
