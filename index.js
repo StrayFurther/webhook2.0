100f459ea3f565535b52bab31c5c5a615235a60b
StrayFurther <t.waldraff95@gmx.de>
2018-10-19 09:13:01 +0200

    an sich selbst schicken

--- index.js
+++ index.js
@@ -93,14 +93,14 @@ app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
       let event = req.body.entry[0].messaging[i];
-      event.sender.id="100029278720161";
+     // event.sender.id="100029278720161";
       let sender = event.sender.id;
       
       console.log("Sender:  "+sender+" type:"+typeof(sender));
       if (event.message && event.message.text) {
         let text = event.message.text
         if (text === 'Generic') {
-            sendGenericMessage(sender)
+            //sendGenericMessage(sender)
             continue
         }
         sendTextMessage(sender, "Message received: " + text.substring(0, 200))
